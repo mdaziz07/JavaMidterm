0d061a7f2645d7fabd133d5598a0109e556768bd
Osman <mdaziz07@gmail.com>
2021-08-31 14:15:32 -0400

    Add DataReader class

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,6 +1,21 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.Stack;
+
 public class DataReader {
+
+    static String absolutePath = System.getProperty("user.dir");
+    static String relativePath = "/src/data/self-driving-car";
+    static final String path = absolutePath + relativePath;
+    static FileReader fileReader;
+    static BufferedReader bufferedReader;
+    //static LinkedList linkedList = new LinkedList<String >();
+    //static Stack stack = new Stack();
+
 
     public static void main(String[] args) {
         /*
@@ -18,7 +33,43 @@ public class DataReader {
          * Use For-Each & While-loop with Iterator to retrieve data.
          */
 
-        String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+        String data = "";
+        LinkedList <String> linkedList = new LinkedList<String >();
+        Stack<String> stack = new Stack<>();
+        String[] line = new String[]{};
+        try {
+            fileReader = new FileReader(path);
+            bufferedReader = new BufferedReader(fileReader);
+
+            try {
+                while ((data = bufferedReader.readLine()) != null) {
+                    System.out.println(data);
+                    line = data.split(" ");
+
+                }
+                System.out.println("\nDone reading file\n");
+                for(String st : line){
+                    System.out.print(st);
+                }
+
+            } catch (Exception e1) {
+                System.out.println("UNABLE TO READ LINE");
+            }
+
+        } catch (FileNotFoundException ex) {
+            System.out.println("FILE NOT FOUND AT " + path);
+
+        } catch (Exception ex) {
+            System.out.println("UNABLE TO OPEN BUFFERED READER ON FILE AT PATH: " + path);
+
+        } finally {
+            try {
+                //assert bufferedReader != null;
+                bufferedReader.close();
+            } catch (Exception e) {
+                System.out.println("UNABLE TO CLOSE BUFFERED READER");
+            }
+        }
 
     }
 
